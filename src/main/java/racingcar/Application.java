55b9e3aa9320cef5a1b20f0eafcc82539da9c9a0
goeka <goeka1337@gmail.com>
2025-10-27 01:24:13 +0900

    feat(application): 자동차 경주 전체 실행 흐름(main) 구현
    - 자동차 이름 입력, 파싱, Car 및 Cars 인스턴스 생성
    - 시도 횟수 입력 후 반복 이동 로직 수행
    - 전체 라운드 실행 결과 및 최종 우승자 출력 구현
    - 입력 검증 및 예외 처리 로직은 추후 추가 예정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,37 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.move.MoveStrategy;
 import racingcar.domain.move.RandomMoveStrategy;
+import racingcar.support.CarNameInputParser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         MoveStrategy moveStrategy = new RandomMoveStrategy();
+        CarNameInputParser carNameInputParser = new CarNameInputParser();
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
         // 자동차 이름 입력
         outputView.printCarNameNotice();
-        String carNames = inputView.readLine();
+        String carNamesInput = inputView.readLine();
 
-        // TODO : 자동차 이름 입력 문자열 검증, 파싱
-        // TODO : Car 인스턴스 모두 생성
-        // TODO : Cars 인스턴스 생성
+        List<String> carNamesList = carNameInputParser.parseCarNames(carNamesInput); // 파싱
+        // TODO : 자동차 이름 입력 문자열 검증,예외처리
+
+        // car 생성 및 리스트에 add
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : carNamesList) {
+            carList.add(new Car(name));
+        }
+        // cars 생성
+        Cars cars = new Cars(carList);
 
         // 시도할 횟수 입력
         outputView.printTryCountNotice();
@@ -25,6 +39,15 @@ public class Application {
 
         // TODO : 시도할 횟수 입력값 검증
 
-        // TODO : 실행결과 출력, 최종우승자 출력
+        // 실행결과 출력
+        outputView.printRaceStartHeader();
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveCars(moveStrategy);
+            outputView.printRoundResult(cars);
+        }
+
+        // 최종우승자 출력
+        outputView.printWinners(cars);
+
     }
 }
